7d07ffdcd0df570e6510e3a0a7203509ad1d936d
Roberto Errico <robertoerrico@MacBook-Pro-di-Roberto.local>
2025-08-13 13:06:23 +0200

    Update Google Sheets Apps Script URL to new deployment

--- backendConfig.js
+++ backendConfig.js
@@ -1,4 +1,4 @@
 // Backend Google Sheets attivo per sincronizzazione multi-dispositivo
-window.sheetsScriptUrl = "https://script.google.com/macros/s/AKfycbxj5yzt825H3Oh4is-0P38NtL5DFVtHk8eieCBGDbB3l9J2Xzp0e05EczkppKtIjphY/exec";
+window.sheetsScriptUrl = "https://script.google.com/macros/s/AKfycbzj6kOOLe9lHWCjP-F3mhhnScoQrqC-qVyuNsUk9nNDZRGKtADkCnbzzig6dUFwrYod/exec";
 
 
